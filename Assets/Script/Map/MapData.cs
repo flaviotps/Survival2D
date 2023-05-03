32b444a55606d692afb1e7613b78ad66e39e70a1
Flavio <flaviotps@gmail.com>
2023-05-03 13:41:14 -0300

    map render changes

--- Assets/Script/Map/MapData.cs
+++ Assets/Script/Map/MapData.cs
@@ -3,62 +3,24 @@ using System.Collections.Generic;
 [System.Serializable]
 public class MapData
 {
-    public Map map;
+    public int width;
+    public int height;
+    public List<MapItem> map;
 
     [System.Serializable]
-    public class Map
+    public class MapItem
     {
-        public string version;
-        public int identifier;
-        public Data data;
+        public int x;
+        public int y;
+        public List<Tile> tiles;
 
         [System.Serializable]
-        public class Data
+        public class Tile
         {
-            public int type;
-            public int version;
-            public int mapWidth;
-            public int mapHeight;
-            public int itemsMajorVersion;
-            public int itemsMinorVersion;
-            public List<Node> nodes;
-
-            [System.Serializable]
-            public class Node
-            {
-                public int type;
-                public string description;
-                public string spawnfile;
-                public string housefile;
-                public List<Feature> features;
-
-                [System.Serializable]
-                public class Feature
-                {
-                    public int type;
-                    public int x;
-                    public int y;
-                    public int z;
-                    public List<Tile> tiles;
-
-                    [System.Serializable]
-                    public class Tile
-                    {
-                        public int type;
-                        public int x;
-                        public int y;
-                        public int tileid;
-                        public List<Item> items;
-
-                        [System.Serializable]
-                        public class Item
-                        {
-                            public int type;
-                            public int id;
-                        }
-                    }
-                }
-            }
+            public int id;
+            public string type;
+            public float imageWidth;
+            public float imageHeight;
         }
     }
 }
--- Assets/Script/TileMap.cs
+++ Assets/Script/TileMap.cs
@@ -1,133 +1,50 @@
-using System;
-using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TileMap : MonoBehaviour
 {
     [SerializeField] private TextAsset textAsset;
-    [SerializeField] private Vector2 groundPivot = new(0f, 0f);
-    [SerializeField] private Vector2 itemPivot = new(0f, 0f);
-
-    // Define the prefab for each tile
+    
     public GameObject tilePrefab;
-    
-    private const int InvertValue = -1;
-    private const float TileSize = 32f;
-    
 
-    // Start is called before the first frame update
     void Start()
     {
         var mapData = CreateMapFromJson(textAsset);
-        // Loop through each tile position and create a new tile game object
-        foreach (var dataNode in mapData.map.data.nodes)
-        {
-            foreach (var feature in dataNode.features)
-            {
-                CreateGround(feature);
-                CreateItems(feature);
-            }
-        }
+        CreateGround(mapData.map);
     }
 
-    private void CreateGround(MapData.Map.Data.Node.Feature feature)
+    private void CreateGround(List<MapData.MapItem> sqms)
     {
-        foreach (var tile in feature.tiles)
+        foreach (var sqm in sqms)
         {
-            var newTexture = Resources.Load<Texture2D>("Textures/" + tile.tileid);
-            
-            var finalX = feature.x + tile.x;
-            var finalY = (feature.y + tile.y) * InvertValue;
-            var finalZ = feature.z;
-            var mapLayer = feature.z;
-            var layer = mapLayer + tile.x + tile.y;
-
-            //Adds offset for textures with width greater than 32 
-            var tileOffSet = Vector3.zero;
-            if (newTexture.width > TileSize)
+         
+            foreach (var tile in sqm.tiles)
             {
-                var xOffset = (newTexture.width - TileSize) / TileSize;
-                tileOffSet = new Vector3(-xOffset, 0, 0);
-            }
-            
-            //Instantiate the tile prefab
-            var position = new Vector3(finalX, finalY, finalZ) + tileOffSet;
-            var newTile = Instantiate(tilePrefab,  position, Quaternion.identity);
-            newTile.name = "[" + tile.x + "," + tile.y + "," + tile.y + "] - ground:" + tile.tileid;
-            //Get the SpriteRenderer component of the new tile GameObject
-            var spriteRenderer = newTile.GetComponent<SpriteRenderer>();
-            spriteRenderer.sortingOrder = layer;
-            
-            //Change the texture of the SpriteRenderer to a new texture
-            spriteRenderer.sprite = Sprite.Create(newTexture,
-                new Rect(0, 0, newTexture.width, newTexture.height), groundPivot);
-
-            // Set the tile game object as a child of the TileMap game object
-            var layerObject = getOrCreateLayerObject(mapLayer.ToString());
-            newTile.transform.parent = layerObject;
-        }
-    }
-
-    private void CreateItems(MapData.Map.Data.Node.Feature feature)
-    {
-        foreach (var tile in feature.tiles)
-        {
-            foreach (var item in tile.items.Select((value, index) => new { value, index }))
-            {
-                var newItemTexture = Resources.Load<Texture2D>("Textures/" + item.value.id);
-
-                var itemX = feature.x + tile.x;
-                var itemY = (feature.y + tile.y) * InvertValue;
-                var itemZ = feature.z;
-                var mapLayer = feature.z;
-                var layer = mapLayer + tile.x + tile.y + item.index;
-
-                //Adds offset for textures with width greater than 32 
-                var tileOffSet = Vector3.zero;
-                if (newItemTexture.width > TileSize)
+                
+                var tileTexture = Resources.Load<Texture2D>("Textures/" + tile.id);
+                if (tileTexture == null)
                 {
-                    var xOffset = (newItemTexture.width - TileSize) / TileSize;
-                    tileOffSet = new Vector3(-xOffset, 0, 0);
+                    Debug.LogError("Failed to load texture for tile ID: " + tile.id);
+                    return;
                 }
-                
-                //Instantiate the item prefab
-                var position = new Vector3(itemX, itemY, -itemZ) + tileOffSet;
-                var newItem = Instantiate(tilePrefab, position, Quaternion.identity);
-                newItem.name = "[" + tile.x + "," + tile.y + "," + tile.y + "] - item:" + item.value.id;
-
-                //Get the SpriteRenderer component of the new item GameObject
-                var itemSpriteRenderer = newItem.GetComponent<SpriteRenderer>();
-                itemSpriteRenderer.sortingOrder = layer;
-                
+                var position = new Vector3(sqm.x, sqm.y * -1) ;
+                var newTile = Instantiate(tilePrefab,  position, Quaternion.identity);
+                newTile.name = "[" + sqm.x + "," + sqm.y + "] - ground:" + tile.id;
+                var spriteRenderer = newTile.GetComponent<SpriteRenderer>();
+                var layer = 1;
+                if (tile.type == "ground")
+                {
+                    layer = 0;
+                }
+                spriteRenderer.sortingOrder = layer;
+            
                 //Change the texture of the SpriteRenderer to a new texture
-                itemSpriteRenderer.sprite = Sprite.Create(newItemTexture,
-                    new Rect(0, 0, newItemTexture.width, newItemTexture.height), itemPivot);
-
-                // Set the item game object as a child of the TileMap game object
-                var layerObject = getOrCreateLayerObject(mapLayer.ToString());
-                newItem.transform.parent = layerObject;
+                spriteRenderer.sprite = Sprite.Create(tileTexture, new Rect(0, 0, tile.imageWidth, tile.imageHeight), Vector2.one/2);
+                newTile.transform.parent = transform;
             }
         }
     }
-
-    private Transform getOrCreateLayerObject(string layerName)
-    {
-    
-        var childTransform = transform.Find(layerName);
-        
-        if (childTransform != null)
-        {
-            return childTransform;
-        }
-
-        var emptyObject = new GameObject
-        {
-            name = layerName,
-            transform = { parent = transform}
-        };
-        return emptyObject.transform;
-    }
-
 
     private static MapData CreateMapFromJson(TextAsset textAsset)
     {
